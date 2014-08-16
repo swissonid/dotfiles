a4c5ad7ce74d02f11d3daa392538439080d3aa49
Patrice Mueller <mueller.patrice@gmail.com>
2014-08-16 13:01:03 +0200

    init commit

---
+++ install_deps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#Get homebrew! Packagemanager for mac os x (http://mxcl.github.io/homebrew/)
+ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+
+#For the c alias (syntax highlighted cat) (Phyton)
+sudo easy_install Pygments%
